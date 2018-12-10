0bc843aa78ee6ae330edb088bec54ed0caccdef8
Samidh Patel <spatel13456@gmail.com>
2018-12-10 22:22:23 +0000

    Automatically backed up by Learn

---
+++ variables.js
@@ -0,0 +1,16 @@
+function speaker() {
+  sentence = "Bird is the word.";
+}
+
+speaker();
+console.log(sentence);
+
+var cuteAnimal = "quokka";
+
+function makeVariable() {
+  var cuteAnimal = "sugar glider";
+  return cuteAnimal;
+}
+
+makeVariable();
+cuteAnimal;
